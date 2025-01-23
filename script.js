ad871f2cffcde527dbe1f1ecec11f5871fe7d7f4
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-23 12:14:45 -0500

    test

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function readFile(file) {
    filetype;
    source;
-   height;
-   width;
-   sendToReader(filetype, source, height, width)
+   sendToReader(filetype, source)
 }
 
-function sendToReader(filetype, source, height, width) {
+function sendToReader(filetype, source) {
    viewer = document.getElementById(viewer);
    // clear class, src, and dimesnion value
    viewer.innerHTML = null;
    // find a way to assign and pass values through if statement
-   viewer.innerHTML = 'class="' + filetype + ' src="' + source + ' height="' + height + ' width="' + width + '"';
+   viewer.innerHTML = 'class="' + filetype + ' src="' + source + '"';
+}
+
+function download(link) {
+   // https://drive.google.com/file/d/1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT/view?usp=sharing
+   // And it extracts the file ID, the part between /d/ and /view, and puts it into a new URL which looks like this:
+   // https://drive.google.com/uc?export=download&id=1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT
+
 }
 
 function message() {
