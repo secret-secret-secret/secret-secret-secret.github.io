938c5e5dc9eb8d960b8036fe34ae6ac29aaf9142
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-23 11:44:13 -0500

    iframe dimensions

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function read(file, source) {
+function readFile(file) {
+   filetype;
+   source;
+   height;
+   width;
+   sendToReader(filetype, source, height, width)
+}
+
+function sendToReader(filetype, source, height, width) {
    viewer = document.getElementById(viewer);
    // clear class, src, and dimesnion value
+   viewer.innerHTML = null;
    // find a way to assign and pass values through if statement
+   viewer.innerHTML = 'class="' + filetype + ' src="' + source + ' height="' + height + ' width="' + width + '"';
+}
 
-   switch (file) {
-      case pdf:
-         viewer.innerHTML = 'class = "pdf" src = "source" width = "" height = ""';
-         break;
-      case picture:
-
-         break;
-      case epub:
-         break;
-      default:
-         break;
-   }
- }
+function message() {
+   alert("Welcome. Keep your prints, faces, and IPs outside of the vehicle");
+}
